cf55debe6fc848e0faec38f6960aa07a17a4111b
DiamondSi <neighborland@outlook.com>
2018-06-20 16:37:21 +0700

    errors fix

--- lists/lists.js
+++ lists/lists.js
@@ -1,43 +1,41 @@
-  var list = [11, 12, 31, 42, 5, 48, 122, 0, 2];
+var list = [11, 12, 31, 42, 5, 48, 122, 0, 2];
 
-    console.log(list.join(", "));
-    list.sort(function (a, b) {
-        return b - a;
-    });
-    console.log(list.join(", "));
+console.log(list.join(", "));
+list.sort(function (a, b) {
+    return b - a;
+});
+console.log(list.join(", "));
 
-    var numbers = 5;
+var numbers = 5;
 
-    var list1 = list.slice(0, numbers);
-    console.log(list1.join(", "));
+var list1 = list.slice(0, numbers);
+console.log(list1.join(", "));
 
-    var list2 = list.slice(list.length - numbers - 1);
-    console.log(list2.join(", "));
+var list2 = list.slice(list.length - numbers);
+console.log(list2.join(", "));
 
 
-    function isEven(arg) {
-        return arg % 2 === 0;
-    }
+function isEven(arg) {
+    return arg % 2 === 0;
+}
 
-    var evenSum = list.reduce(function (sum, current) {
-        if (isEven(current)) {
-            sum += current;
-        }
-        return sum;
-    }, 0);
-    console.log(evenSum);
+var evenSum = list.reduce(function (sum, current) {
+    if (isEven(current)) {
+        return sum += current;
+    } else return sum;
+}, 0);
+console.log(evenSum);
 
-    var list100 = [];
-    console.log(list100.join(", "));
-    for (var i = 1; i <= 100; i++) {
-        list100.push(i - 1);
-    }
-    console.log(list100.join(", "));
+var list100 = [];
+console.log(list100.join(", "));
+for (var i = 1; i <= 100; i++) {
+    list100.push(i - 1);
+}
+console.log(list100.join(", "));
 
-    var evenSum2 = list100.reduce(function (sum, current) {
-        if (isEven(current)) {
-            sum += current;
-        }
-        return sum;
-    }, 0);
-    console.log(evenSum2);
+var evenSum2 = list100.reduce(function (sum, current) {
+    if (isEven(current)) {
+        return sum += current;
+    } else return sum;
+}, 0);
+console.log(evenSum2);
