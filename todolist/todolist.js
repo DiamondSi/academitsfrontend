534385d87bf2a5621973603b196ace96788f0abb
Dmitry Dubchak <neighborland@outlook.com>
2018-06-20 07:43:21 +0700

    lists

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -2,16 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     var addTaskButton = document.getElementById("add-task-button");
     var taskName = document.getElementById("task-name-input");
     var taskList = document.getElementById("task-list");
+
     addTaskButton.addEventListener("click", function () {
         var textValue = taskName.value;
         var taskListItem = document.createElement("li");
+
         var taskDelButton = document.createElement("button");
         taskDelButton.type = "reset";
         taskDelButton.textContent = "Удалить";
-        taskDelButton.setAttribute("class", "del-task-button");
+        taskDelButton.className="del-task-button";
+
         taskDelButton.addEventListener("click", function () {
             taskListItem.remove();
         });
+
         taskListItem.innerText = textValue;
         taskListItem.appendChild(taskDelButton);
         taskList.appendChild(taskListItem);
