8788e1a23f0162ad1406feee63f623d09e77963e
DiamondSi <neighborland@outlook.com>
2018-06-21 11:23:18 +0700

    result output

--- quadratic/quadratic.js
+++ quadratic/quadratic.js
@@ -8,30 +8,41 @@ document.addEventListener("DOMContentLoaded", function () {
         var x;
         var discriminant = b * b - 4 * a * c;
 
-        var result;
+        var bOutput = "+" + b;
+        if (b < 0) {
+            bOutput = "-" + Math.abs(b);
+        }
+        if (b === -1) {
+            bOutput = "-";
+        }
+        var cOutput = "+" + c;
+        if (c < 0) {
+            cOutput = "-" + Math.abs(c);
+        }
+
+        var result = a + "x<sup>2</sup> " + bOutput + "x " + cOutput + " = 0";
 
         if (a === 0 && b === 0 && c === 0) {
-            result = a + "*x2 + " + b + "*x + " + c + " -> 0 = 0. Это не квадратное уравнение. x - любоe число.";
+            result += " -> Это не квадратное уравнение. x - любоe число.";
         } else if (a === 0 && b === 0) {
-            result = a + "*x2 + " + b + "*x +" + c + " = 0 неверное математическое выражение";
+            result += " -> неверное математическое выражение";
         } else if (a === 0) {
             x = -c / b;
-            result = a + "*x2 + " + b + "*x +" + c + " = 0 -> " + b + "x + " + c + " = 0 Это не квадратное уравнение. x = " + x;
+            result += " -> Это не квадратное уравнение. x = " + x;
         } else if (discriminant < 0) {
-            result = "Дискриминант = " + discriminant + " Корней, на множестве действительных чисел, нет.";
+            result += ". Дискриминант = " + discriminant + " Корней, на множестве действительных чисел, нет.";
         } else if (discriminant === 0) {
             x = -b / (2 * a);
             if (b === 0) {
                 x = 0;
             }
-            result = "Дискриминант = " + discriminant + " x1 = x2 = " + x;
+            result += ". Дискриминант = " + discriminant + ", x1 = x2 = " + x;
         } else {
             var x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             var x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            result = "Дискриминант = " + discriminant + " x1 = " + x1 + ", x2 = " + x2;
+            result += ". Дискриминант = " + discriminant + ", x1 = " + x1 + ", x2 = " + x2;
         }
-        var resultOutput = document.createElement("label");
-        resultOutput.innerText = result;
-        document.body.appendChild(resultOutput);
+        var resultOutput = document.getElementById("result");
+        resultOutput.innerHTML = result;
     });
 });
