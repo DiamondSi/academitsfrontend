664e5611e4edb3324759d04b034b034e37df3cd0
DiamondSi <neighborland@outlook.com>
2018-07-25 14:43:28 +0700

    Update index.js

--- underscoretask/js/index.js
+++ underscoretask/js/index.js
@@ -1,48 +1,37 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var calculateButton = document.getElementById("calculate-button");
-    calculateButton.addEventListener("click", function () {
-        var a = parseFloat(document.getElementById("ka").value);
-        var b = parseFloat(document.getElementById("kb").value);
-        var c = parseFloat(document.getElementById("kc").value);
+document.addEventListener("DOMContentLoaded", function() {
 
-        var x;
-        var discriminant = b * b - 4 * a * c;
+    function Person(name, lastName, age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
-        var bOutput = "+" + b;
-        if (b < 0) {
-            bOutput = "-" + Math.abs(b);
-        }
-        if (b === -1) {
-            bOutput = "-";
-        }
-        var cOutput = "+" + c;
-        if (c < 0) {
-            cOutput = "-" + Math.abs(c);
-        }
+    var persons = [];
+    var personsCount = 10;
 
-        var result = a + "x<sup>2</sup> " + bOutput + "x " + cOutput + " = 0";
+    for (var i = 0; i < personsCount; i++) {
+        persons.push(new Person("Name" + i, "LastName" + i, i * 2 + 16));
+    }
 
-        if (a === 0 && b === 0 && c === 0) {
-            result += " -> Это не квадратное уравнение. x - любоe число.";
-        } else if (a === 0 && b === 0) {
-            result += " -> неверное математическое выражение";
-        } else if (a === 0) {
-            x = -c / b;
-            result += " -> Это не квадратное уравнение. x = " + x;
-        } else if (discriminant < 0) {
-            result += ". Дискриминант = " + discriminant + " Корней, на множестве действительных чисел, нет.";
-        } else if (discriminant === 0) {
-            x = -b / (2 * a);
-            if (b === 0) {
-                x = 0;
-            }
-            result += ". Дискриминант = " + discriminant + ", x1 = x2 = " + x;
-        } else {
-            var x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            var x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            result += ". Дискриминант = " + discriminant + ", x1 = " + x1 + ", x2 = " + x2;
-        }
-        var resultOutput = document.getElementById("result");
-        resultOutput.innerHTML = result;
+    // посчитать средний возраст всех людей
+    var averageAge = _.reduce(persons, function(memo, person) {
+        return memo + person.age;
+
+    }, 0) / persons.length;
+    console.log("Средний возраст: " + averageAge);
+
+
+    // Получить список людей с возрастом от 20 до 30 включительно, отсортировать их по возрастанию возраста
+    var filteredPersons = _.chain(persons).filter(function(person) {
+        return person.age >= 20 && person.age <= 30;
+    }).sortBy("age").value();
+    console.log("Список людей с возрастом от 20 до 30 включительно, отсортирон по возрастанию возраста");
+    console.log(filteredPersons);
+
+    // Добавить всем людям поле fullName, которое состоит из фамилии и имени через пробел
+    _.each(persons, function(person) {
+        person.fullName = person.lastName + " " + person.name;
     });
+    console.log("Добавил всем людям поле fullName, которое состоит из фамилии и имени через пробел");
+    console.log(persons);
 });
